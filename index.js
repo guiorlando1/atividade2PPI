5588bd3f180ad7642590f659b9db6e635eab36e0
guiorlando1 <guicsus@gmail.com>
2024-05-15 17:55:33 -0300

    finalizando

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ app.use('/listarUsuario', (req,resp) =>{
     resp.write('</body>');
     resp.write('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>');
     resp.write('</html>');
+    resp.end();
 });
 
 app.listen(porta, host, () => {
